9ca603e73201de1acddcd971fcf4a31418fd2762
jeoncook <vegitosayan123@gmail.com>
2022-07-17 11:46:07 +0200

    e

--- main.py
+++ main.py
@@ -1873,18 +1873,34 @@ async def off(ctx):
   if (ctx.message.author.id == 358594990982561792) or (ctx.message.author.id == 891449503276736512):
     await ctx.message.delete()
     t_channel = bot.get_channel(918146416747102249)
-    t_msg = await t_channel.fetch_message(998160266418196551)
-    view = Off_AUTO_CRYPTO_Tickets()
-    await t_msg.edit(view=view)
+    msgs = await t_channel.history(limit=None).flatten()
+    newmsg = None
+    for msg in msgs:
+      if msg.embeds:
+        if len(msg.embeds) == 2:
+          if msg.components:
+            if msg.components[0].children[0].custom_id == "auto_crypto_ticket":
+              newmsg = msg
+    if newmsg == None:
+      return
+    await newmsg.edit(view=Off_AUTO_CRYPTO_Tickets())
 
 @bot.command()
 async def on(ctx):
   if (ctx.message.author.id == 358594990982561792) or (ctx.message.author.id == 891449503276736512):
     await ctx.message.delete()
     t_channel = bot.get_channel(918146416747102249)
-    t_msg = await t_channel.fetch_message(998160266418196551)
-    view = AUTO_CRYPTO_Tickets()
-    await t_msg.edit(view=view)
+    msgs = await t_channel.history(limit=None).flatten()
+    newmsg = None
+    for msg in msgs:
+      if msg.embeds:
+        if len(msg.embeds) == 2:
+          if msg.components:
+            if msg.components[0].children[0].custom_id == "auto_crypto_ticket":
+              newmsg = msg
+    if newmsg == None:
+      return
+    await newmsg.edit(view=AUTO_CRYPTO_Tickets())
 
 class Off_AUTO_CRYPTO_Tickets(discord.ui.View):
   def __init__(self):
